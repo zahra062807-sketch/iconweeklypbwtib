0f116c1dc0242fa423d19a2ea6056c8bb7b2a328
zahra062807-sketch <zahra062807@gmail.com>
2026-09-22 15:21:24 +0700

    route, view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEKNOLOGI INFOMASI</title>
+</head>
+<body>
+    <h1>HALAMAN HOME</h1>
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>profile</title>
+</head>
+<body>
+    <h1>HALAMAN PROFILE</h1>
+    <p>
+        Nama : Icon <br>
+        Prodi : TI 
+    </p>
+    <script src="js/alert.js"></script>
+</body>
+</html>
